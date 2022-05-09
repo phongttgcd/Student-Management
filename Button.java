ec823c1a102b78838ffce20b9fb8b4af11695bc5
phongttgcd <100106529+phongttgcd@users.noreply.github.com>
2022-05-09 10:12:24 +0700

    Add files via upload

---
+++ Button.java
@@ -0,0 +1,15 @@
+package com.hello;
+
+import javax.swing.*;
+
+abstract class Button extends JButton {
+
+    private String buttonText;
+
+    public Button(String text) {
+        this.buttonText = text;
+        this.setText(this.buttonText);
+        this.setFocusable(false);
+    }
+}
+
